35e3ea709c285101c591f1e8a34bfc5b86fdd07f
wouow <aa3b3c2tw@gmail.com>
2023-10-26 19:09:12 +0800

    complete

--- Assets/FloorManager.cs
+++ Assets/FloorManager.cs
@@ -10,7 +10,11 @@ public class FloorManager : MonoBehaviour
     {
         int type = Random.Range(0, FloorPrefabs.Length);
         GameObject floor = Instantiate(FloorPrefabs[type], transform);
+        Destroy(floor.GetComponent<BoxCollider2D>());
+        floor.AddComponent<BoxCollider2D>();
         floor.transform.position = new Vector3(Random.Range(-8f,8f),-6f ,0f);
         floor.transform.localScale = new Vector3(Random.Range(1f,3f), 0.5f, 0f);
+
+        
     }
 }
